fc744601ec9231d260e13d6b89680f0a50efe412
Emilia <reel.emilia@gmail.com>
2025-12-03 00:14:12 -0500

    Formatted more citations and fixed some typos in index.js. MH Sources are half done.

--- index.js
+++ index.js
@@ -3,149 +3,149 @@ const sources = [
     id: 0, 
     category:'highlight',
     title: 'r/transontario Gender Affirming Care Spreadsheet',
-    url: 'https://docs.google.com/spreadsheets/d/111o63Tvt20heohe9KtRW_AZPBKOz4kpZ1pb7r0CJpvU/edit?gid=0#gid=0',
-    location: 'Online',
-    descritpion: '',
+    url: 'https://docs.google.com/spreadsheets/d/111o63tvt20heohe9ktrw_azpbkoz4kpz1pb7r0cjpvu/edit?gid=0#gid=0',
+    location: 'online',
+    description: 'test',
   },
   {
     id: 1,
     category:'highlight',
     title: 'Rainbow Health Directory',
     url: 'https://www.rainbowhealthontario.ca/2slgbtq-health/service-provider-directory/',
-    location: 'Online',
-    description: 'This directory is designed to help you find health and social service providers who have expressed a commitment to providing competent and welcoming care to 2SLGBTQ people in Ontario.',
+    location: 'online',
+    description: 'This directory is designed to help you find health and social service providers who have expressed a commitment to providing competent and welcoming care to 2SLGBTQ people in ontario. (Taken from website)',
   },
   { id: 2,
     category:'highlight',
     title: 'HQ Toronto',
     url: 'https://hqtoronto.ca/',
-    location: '790 Bay St. #820, Toronto, ON M5G 1N8',
-    description: 'HQ Toronto is a Healthcare center near College Station that has a variety of resouces from legal to clinical. Express STD testing, mental and social health services, alongside a once-a-month Trans I.D. legal clinic.',
+    location: '790 Bay st. #820, Toronto, ON M5G 1N8',
+    description: 'HQ Toronto is a healthcare center near College Station that has a variety of resouces from legal to clinical. Express STD testing, mental and social health services, alongside a once-a-month trans ID legal clinic.',
   },
   { id: 3,
     category:'highlight',
-    title: 'The 519',
+    title: 'the 519',
     url: 'https://www.the519.org/',
     location: '519 Church Street, Toronto, ON M4Y 2C9',
-    description: 'Major LGBTQ hub in the center of The Village. Services and supports in many different areas. Financial, newcomers to Canada, housing, family and children, health services, art and culture, and social events that happen every month.',
+    description: 'Major LGBTQ hub in the center of the village. Services and supports in many different areas. financial, newcomers to Canada, housing, family and children, health services, art and culture, and social events that happen every month.',
   },
   { id: 4,
     category:'social',
     title: 'Friends of Ruby',
     url: 'https://www.friendsofruby.ca/',
     location: '489 Queen St E LL01, Toronto, ON M5A 1V1',
-    description: 'Firends of Ruby is a drop-in youth centre for 2SLGBTQIA+ individuals, ages 16-29. They provide meals, activities, support groups, free one-on-one counselling, and assistance with housing, healthcare and jobs.',
+    description: 'Friends of Ruby is a drop-in youth centre for 2SLGBTQIA+ individuals, ages 16-29. They provide meals, activities, support groups, free one-on-one counselling, and assistance with housing, healthcare and jobs.',
   },
   { id: 5,
-    category:'',
+    category:'social',
+    title: 'Sherbourne Health Programs',
+    url: 'https://sherbourne.on.ca/community-health-programs/community-groups/',
+    location: '333 Sherbourne St, Toronto, ON M5A 2S5',
+    description: 'Sherbourne Hospital in Toronto has a wide variety of social programs for queer individuals.',
+  },
+  {
+    id: 6, 
+    category:'social',
+    title: 'Gilbert Centre',
+    url: 'https://www.gilbertcentre.ca/',
+    location: '80 Bradford St Suite 525, Barrie, ON L4N 3A8',
+    description: '',
+  },
+  {
+    id: 7, 
+    category:'social',
+    title: 'TransQ',
+    url: 'https://transq.ca/',
+    location: '80 Bradford St Suite 525, Barrie, ON L4N 3A8',
+    description: '',
+  },
+  {
+    id: 8, 
+    category:'social',
+    title: 'Simcoe County',
+    url: 'https://www.simcoepride.com/',
+    location: 'Simcoe Pride',
+    description: '',
+  },
+  {
+    id: 9, 
+    category:'social',
     title: '',
     url: '',
     location: '',
     description: '',
   },
   {
-    id: 6, 
-    category:'',
-    title: '',
-    url: '',
-    location: '',
-    descritpion: '',
-  },
-  {
-    id: 7, 
-    category:'',
-    title: '',
-    url: '',
-    location: '',
-    descritpion: '',
-  },
-  {
-    id: 8, 
-    category:'',
-    title: '',
-    url: '',
-    location: '',
-    descritpion: '',
-  },
-  {
-    id: 9, 
-    category:'',
-    title: '',
-    url: '',
-    location: '',
-    descritpion: '',
-  },
-  {
     id: 10, 
-    category:'',
-    title: '',
-    url: '',
-    location: '',
-    descritpion: '',
+    category:'social',
+    title: 'Gender Diverse Indigenous Mentor Program',
+    url: 'https://www.youthline.ca/support_near_you/gender-diverse-indigenous-mentor-nogojiwanong-friendship-centre/',
+    location: '580 Cameron St, Peterborough, ON K9J 3Z5',
+    description: '',
   },
   {
     id: 11, 
-    category:'',
-    title: '',
-    url: '',
-    location: '',
-    descritpion: '',
+    category:'social',
+    title: 'Sherbourne Health: Supporting Our Youth (SOY)',
+    url: 'https://sherbourne.on.ca/supporting-our-youth-soy/',
+    location: '333 Sherbourne St, Toronto, ON M5A 2SS',
+    description: '',
   },
   {
     id: 12, 
-    category:'',
-    title: '',
+    category:'mental',
+    title: 'Beyond Trauma',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   },
   {
     id: 13,
-    category:'',
-    title: '',
+    category:'mental',
+    title: 'Ontario Structured Psychotherapy Programs',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   },
   {
     id: 14, 
-    category:'',
-    title: '',
+    category:'mental',
+    title: 'Bounce Back',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   },
   {
     id: 15, 
-    category:'',
-    title: '',
+    category:'mental',
+    title: 'Gilbert Centre',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   },
   {
     id: 16, 
-    category:'',
-    title: '',
+    category:'mental',
+    title: 'Catholic Family Services Simcoe County',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   },
   {
     id: 17, 
-    category:'',
-    title: '',
+    category:'mental',
+    title: 'CMHA',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   },
   {
     id: 18, 
-    category:'',
-    title: '',
+    category:'mental',
+    title: 'LGBTQ Youthline',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   },
   {
     id: 19, 
@@ -153,7 +153,7 @@ const sources = [
     title: '',
     url: '',
     location: '',
-    descritpion: '',
+    description: '',
   }
 ];
 
@@ -162,7 +162,7 @@ let socialSources = ``;
 let mentalSources = ``;
 let medicalSources = ``;
 let familySources = ``;
-let harmhreductSources = ``;
+let harmreductSources = ``;
 let financialfoodSources = ``;
 
 sources.forEach(function (source) {
@@ -193,6 +193,14 @@ let cardTemplate = `
 
   }
   console.log(socialSources)
+  if (source.category === 'mental') {
+  mentalSources =
+    mentalSources+
+    cardTemplate;
+
+  }
+  console.log(mentalSources)
+
 });
 
 const highlightcards = document.querySelector('.highlighted-resources');
@@ -200,6 +208,9 @@ highlightcards.innerHTML = '<h2>Highlighted Resources</h2>' + highlightedSources
 
 const socialcards = document.querySelector('.social-resources');
 socialcards.innerHTML = '<h2>Social Resources</h2>' + socialSources;
+
+const mentalcards = document.querySelector('.mental-resources');
+mentalcards.innerHTML = '<h2>Mental Health Resources</h2>' + mentalSources;
 
 
 
