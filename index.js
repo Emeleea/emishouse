af5c74d3663dcb6f395e668ad36992e6c9586fae
Erik Hunter <6209745-Blaziken_@users.noreply.gitlab.com>
2026-04-18 23:32:06 -0400

    Erik overhaul v3 - Added auto-generating filters for category and region

--- index.js
+++ index.js
@@ -6,103 +6,129 @@ async function fetchJSONData() {
 const sources = fetchJSONData();
 const resources = [];
 
-const tpl = document.getElementById('item-template');
-const container = document.getElementById('item-container')
-let selected_location = "any"
-let selected_type = "any"
+let selected_region = "Any"
+let selected_type = "Any"
+
+const categories = new Set();
+const regions = new Set();
 
 class Resource {
-  constructor(url, categories, regions, title, location, description) {
+  constructor(url, categories, regions, title, address, description) {
     this.url = url;
     this.categories = categories;
     this.regions = regions;
     this.title = title;
-    this.location = location;
+    this.address = address;
     this.description = description;
   }
 }
-
-// TODO: Change this into a set, and populate it during the initial parsing
-// of the main resources list
-const categories = [
-  "highlight",
-  "list",
-  "social",
-  "mental",
-  "medical",
-  "legal",
-  "hrt",
-  "family",
-  "harmreduct",
-  "housing",
-  "financial",
-  "food",
-]
-
-// TODO: Do the same as above, but also with locations
 
 // This is just functional-style syntax for a `for loop`, makes chaining
 // operations together easier
 sources
   .then(data => data.forEach(i => {
+    if (i.categories) i.categories.forEach(i => categories.add(i))
+    if (i.regions) i.regions.forEach(i => regions.add(i))
     resources.push(new Resource(
       i.url,
       i.categories,
       i.regions,
       i.title,
-      i.location,
+      i.address,
       i.description
     ));
   }))
   .then(() => {
     // this function is here because I need it to render the page *after* the
     // resources array is initialized
-    renderIntoTemplate(container, tpl, "any", "any");
+    renderIntoTemplate(item_container, item_template, "Any", "Any");
+
+    const category_template = document.getElementById("category-filter-template")
+    const category_container = document.getElementById("category-filter-container")
+
+    const region_template = document.getElementById("region-filter-template")
+    const region_container = document.getElementById("region-filter-container")
+
+    renderCategoryFilters(category_container, category_template, categories)
+    renderRegionFilters(region_container, region_template, regions)
   })
 
-function renderIntoTemplate(container, template, selected_category, selected_location) {
-  container.innerHTML = '';
+function renderCategoryFilters(container, template, set) {
+  let sorted = Array.from(set).sort();
+  sorted.unshift("Any");
 
-  const matches = resources
-    .filter(i => {
-      if (i.categories) {
-        return selected_category === "any" ? true : i.categories.includes(selected_category)
-      }
-    })
-    .filter(i => {
-      if (i.regions) {
-        return selected_location === "any" ? true : i.regions.includes(selected_location)
-      }
-      else {
-        return false
-      }
-    })
-
-  matches.forEach(i => {
+  sorted.forEach(item => {
     const clone = template.content.cloneNode(true);
-    const headingEl = clone.querySelector('.card-heading')
-    const title_linkEl = clone.querySelector('.card-title-link')
-    const locationEl = clone.querySelector('.card-location')
-    const descriptionEl = clone.querySelector('.card-description')
+    const input_element = clone.getElementById('category-input')
+    const label_element = clone.getElementById('category-label')
 
-    if (headingEl) headingEl.textContent = location ?? ''
-    if (title_linkEl) title_linkEl.setAttribute("href", i.url ?? '')
-    title_linkEl.textContent = i.title ?? ''
-    if (locationEl) locationEl.textContent = i.location ?? ''
-    if (descriptionEl) descriptionEl.textContent = i.description ?? ''
+    if (input_element) input_element.setAttribute("value", item)
+    if (input_element) input_element.setAttribute("id", `category-${item}`)
+    if (label_element) label_element.textContent = item
+    if (label_element) label_element.setAttribute("for", `category-${item}`)
+
+    // Special Case:
+    if (input_element && item === "Any") input_element.setAttribute("checked", true)
 
     container.appendChild(clone);
   })
 }
 
-const resource_form = document.getElementById("resource-filter-form");
+function renderRegionFilters(container, template, set) {
+  let sorted = Array.from(set).sort();
+  sorted.unshift("Any")
+
+  sorted.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const input_element = clone.getElementById('region-input')
+    const label_element = clone.getElementById('region-label')
+
+    if (input_element) input_element.setAttribute("value", item)
+    if (input_element) input_element.setAttribute("id", `region-${item}`)
+    if (label_element) label_element.textContent = item
+    if (label_element) label_element.setAttribute("for", `region-${item}`)
+
+    // Special Case:
+    if (input_element && item === "Any") input_element.setAttribute("checked", true)
+
+    container.appendChild(clone);
+  })
+}
+
+function renderIntoTemplate(container, template, selected_category, selected_region) {
+  container.innerHTML = '';
+
+  const matches = resources
+    .filter(i => i.categories || selected_category === "Any" ? selected_category === "Any" || i.categories.includes(selected_category) : false)
+    .filter(i => i.regions || selected_region === "Any" ? selected_region === "Any" || i.regions.includes(selected_region) : false)
+
+  matches.forEach(i => {
+    const clone = template.content.cloneNode(true);
+    const title_url_element = clone.querySelector('.card-title-url')
+    const region_url_element = clone.querySelector('.card-region-url')
+    const description_element = clone.querySelector('.card-description')
+
+    if (title_url_element) title_url_element.setAttribute("href", i.url ?? '')
+    if (title_url_element) title_url_element.textContent = i.title ?? ''
+    if (region_url_element) region_url_element.textContent = i.address ?? ''
+    if (region_url_element) region_url_element.setAttribute("href", `https://maps.google.com/?q=${i.address}`)
+    if (description_element) description_element.textContent = i.description ?? ''
+
+    container.appendChild(clone);
+  })
+}
+
+const item_template = document.getElementById('item-template');
+const item_container = document.getElementById('item-container')
+
+const resource_form = document.getElementById("category-filter-container");
 resource_form.addEventListener('change', (e) => {
   selected_type = e.target.value
-  renderIntoTemplate(container, tpl, selected_type, selected_location)
+  renderIntoTemplate(item_container, item_template, selected_type, selected_region)
 })
 
-const location_form = document.getElementById("location-filter-form");
-location_form.addEventListener('change', (e) => {
-  selected_location = e.target.value
-  renderIntoTemplate(container, tpl, selected_type, selected_location)
+const region_form = document.getElementById("region-filter-container");
+region_form.addEventListener('change', (e) => {
+  selected_region = e.target.value
+  renderIntoTemplate(item_container, item_template, selected_type, selected_region)
 })
